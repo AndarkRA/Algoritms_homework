ff3b9cf89dd09e81b00844fe538b9b02bfe5dff4
AndarkRA <klushin1973@yandex.ru>
2021-12-26 21:57:51 +0300

    ДЗ 06

--- Homeworks/Homeworks/Program.cs
+++ Homeworks/Homeworks/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using HW5_1;
+﻿/*using System;
+using HW6_1;
 using System.Collections;
 
-namespace HW5_1
+namespace HW6_1
 {
 
     class Program
@@ -34,6 +34,37 @@ namespace HW5_1
 
             myTree.printTreeDFS();
             Console.WriteLine();
+        }
+    }
+} */
+
+
+using System;
+
+namespace HW6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graf myGraf = new Graf();
+            myGraf.AddVertex(15);
+            myGraf.AddVertex(25);
+            myGraf.AddVertex(35);
+            myGraf.AddVertex(45);
+            myGraf.AddVertex(55);
+
+            myGraf.AddEdge(15, 25, 10);
+            myGraf.AddEdge(15, 35, 8);
+
+            myGraf.AddEdge(45, 25, 7);
+            myGraf.AddEdge(55, 35, 6);
+            myGraf.AddEdge(45, 55, 5);
+
+            myGraf.printGrafBFS();
+            myGraf.printGrafDFS();
+
+            Console.ReadLine();
         }
     }
 }
